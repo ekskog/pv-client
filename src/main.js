5e792ee8992c5e81586b8294723e49320228da57
Tayo Carvalhal <root@hbvu.su>
2025-10-27 09:11:46 +0100

    changed he action to monitor nginx.conf

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// force rebuild on 24/10/2025 - 17:30
+// force rebuild on 27/10/2025 - 09:11
 import { createApp } from 'vue'
 //import './assets/style.css'
 import '@fortawesome/fontawesome-free/css/all.css'
